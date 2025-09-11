dd868f2c217dc59ed02a49e1df2b5cf987c807c4
Krish-0089 <kambojkrish941@gmail.com>
2025-09-11 18:34:58 +0530

    Create printing odd numbers code.c

---
+++ printing odd numbers code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    for(int i=1; i<= num; i++){
+        if(i%2==0){
+            continue;
+        }
+        printf("%d\n",i);
+    }
+    return 0;
+}
