07aa10b9d8d01f10a4aae0af9c28c6fedacb076f
Krish-0089 <kambojkrish941@gmail.com>
2025-09-11 18:34:00 +0530

    Create Printing natural numbers expect the multiple of 5 code.c

---
+++ Printing natural numbers expect the multiple of 5 code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    for(int i=1; i<= num; i++){
+        if(i%5==0){
+            continue;
+        }
+        printf("%d\n",i);
+    }
+    return 0;
+}
